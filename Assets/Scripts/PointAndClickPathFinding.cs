056d62f1914ee64c7061fc176b27eb87fb313c7d
Janak <Janakkumar.Bhanushal@ryerson.ca>
2018-03-28 19:19:31 -0400

    finished pathfinding2, pathfinding1 still needs some calculus for jumps

---
+++ Assets/Scripts/PointAndClickPathFinding.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClickPathFinding : MonoBehaviour {
+
+	public	GameObject		clickPoint;
+	private Transform		clickPointT;
+
+	private Ray				ray;
+	private RaycastHit		hit;
+	private LayerMask		layerMask;
+	private RaycastHit2D	hit2D;
+	private Transform		t;
+	private Rigidbody2D		rb;
+	private Vector2			destination;
+	private bool			destinationInitalzied;
+	private float			directionX;
+
+	public float			speed;
+	public float			marginOfError;
+
+	// Use this for initialization
+	void Start () {
+		clickPointT =	clickPoint.GetComponent<Transform>();
+		t =				GetComponent<Transform>();
+		rb =			GetComponent<Rigidbody2D>();
+		layerMask =		1 << 9;
+		layerMask =		~layerMask;
+		destinationInitalzied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Input.GetMouseButtonDown(0))
+			findDirectionAndDestination();
+		
+		if (destinationInitalzied)
+		{
+			float distanceX = Mathf.Abs(t.position.x - destination.x);
+			float distanceY = Mathf.Abs(t.position.y - destination.y);
+
+			if (distanceX > marginOfError)
+			{
+				if (distanceX > distanceY)
+					transform.Translate(speed * directionX * Time.deltaTime, 0, 0);
+			}
+			else destinationInitalzied = false;
+		}
+	}
+
+	private void findDirectionAndDestination()
+	{
+		if (Physics.Raycast(ray, out hit, 100))
+		{
+			clickPointT.position = hit.point;
+			hit2D = Physics2D.Raycast(hit.point, Vector2.down, 100, layerMask);
+			if (hit2D)
+			{
+				directionX = hit2D.point.x / Mathf.Abs(hit2D.point.x);
+				destination = new Vector2(hit2D.point.x, t.position.y);
+				destinationInitalzied = true;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PointAndClickPathFinding2.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClickPathFinding2 : MonoBehaviour {
+
+	private Ray ray;
+	private RaycastHit hit;
+	private LayerMask layerMask;
+	private Transform t;
+	private bool destinationInitalzied;
+	private Vector3 destination;
+	private Vector3 direction;
+	private Vector3 distance;
+
+	public float speed;
+	public float marginOfError;
+
+	// Use this for initialization
+	void Start () {
+		t = GetComponent<Transform>();
+		layerMask = 1 << 9;
+		layerMask = ~layerMask;
+		destinationInitalzied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Input.GetMouseButtonDown(0))
+			findDirectionAndDestination();
+
+		if (destinationInitalzied)
+		{
+			float absDistanceX = Mathf.Abs(distance.x);
+			float absDistanceY = Mathf.Abs(distance.y);
+
+			float currAbsDistanceX = Mathf.Abs(t.position.x - destination.x);
+			float currAbsDistanceY = Mathf.Abs(t.position.y - destination.y);
+
+
+			if (currAbsDistanceX > marginOfError || currAbsDistanceY > marginOfError)
+			{
+				float totalAbsDistance = (absDistanceX + absDistanceY);
+				float distanceXPercentage = absDistanceX / totalAbsDistance;
+				float distanceYPercentage = absDistanceY / totalAbsDistance;
+				float translateX = speed * distanceXPercentage * direction.x * Time.deltaTime;
+				float translateY = speed * distanceYPercentage * direction.y * Time.deltaTime;
+				t.Translate(translateX, translateY, 0);
+			}
+			else destinationInitalzied = false;
+		}
+	}
+
+	private void findDirectionAndDestination()
+	{
+		if (Physics.Raycast(ray, out hit, 100, layerMask))
+		{
+			destination = hit.point;
+			destinationInitalzied = true;
+			distance = hit.point - t.position;
+			float directionX = distance.x / Mathf.Abs(distance.x);
+			float directionY = distance.y / Mathf.Abs(distance.y);
+			direction = new Vector3(directionX,	directionY);
+		}
+	}
+}
