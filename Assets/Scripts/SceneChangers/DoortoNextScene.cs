4c10f859963559b6a0eeec8ebfeb030db50391dd
Janak <Janakkumar.Bhanushal@ryerson.ca>
2018-05-10 09:57:35 -0400

    minor adjustments to script DoortoNextScene

--- Assets/Scripts/SceneChangers/DoortoNextScene.cs
+++ Assets/Scripts/SceneChangers/DoortoNextScene.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoortoNextScene : MonoBehaviour {
+public class DoorToNextScene : MonoBehaviour {
+	public string nextScene;
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Player")
 		{
-			FadeManager.Instance.Fade(true, 1.5f, 1, "Scene1");
+			FadeManager.Instance.Fade(true, 1.5f, 1, nextScene);
 		}
 	}
 }
